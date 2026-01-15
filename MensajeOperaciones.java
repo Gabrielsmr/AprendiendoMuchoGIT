2546b7e66b9c00d404985fd5303e80e5296832a2
Gabrielsmr <gabgongar2@alu.edu.gva.es>
2026-01-15 12:25:43 +0100

    Create MensajeOperaciones.java

---
+++ MensajeOperaciones.java
@@ -0,0 +1,8 @@
+public class MensajeOperaiones{
+  public void MostrarMensajeInicio(){
+    System.out.println("Bienvenid@ al programa de Operaciones");
+  }
+  public void MostrarMensajeFin(){
+    System.out.println("Gracias por usar programa operaciones")
+  }
+}  
