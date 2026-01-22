b17bf83941bfe9767fe9cbf5c1c71c28e9d24335
Gabrielsmr <gabgongar2@alu.edu.gva.es>
2026-01-22 12:33:07 +0100

    ultimo commit

--- MensajeOperaciones.java
+++ MensajeOperaciones.java
@@ -1,8 +1,8 @@
-public class MensajeOperaiones{
+public class MensajeOperaciones{
   public void MostrarMensajeInicio(){
     System.out.println("Bienvenid@ al programa de Operaciones");
   }
   public void MostrarMensajeFin(){
-    System.out.println("Gracias por usar programa operaciones")
+    System.out.println("Gracias por usar programa operaciones");
   }
 }  
